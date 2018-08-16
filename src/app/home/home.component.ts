43d0866a2d5c7e3b28c9c10c86fd1c68b479ade9
Ivan Vitali <webcoder.ivan@gmail.com>
2018-08-16 18:05:59 -0300

    se trabajo el 2 way data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
+import { Employee } from '../employee/employee.model';
 
 @Component({
   selector: 'home',
   styleUrls: ['./home.component.css'],
   templateUrl: './home.component.html'
 })
+
 export class HomeComponent {
   languages = ['English', 'Spanish', 'Other'];
+  employee = new Employee('', '',false,'','','');
+
 }
+
+
+
