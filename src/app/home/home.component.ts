b5cec811d00ea0a7e6c69f72f8ef48b364324040
Ivan Vitali <webcoder.ivan@gmail.com>
2018-08-18 10:48:00 -0300

    se trabajo el enlace doble entre el modelo employee y el formulario de angular, tambien se creo el metodo firstNameToUpperCase() que se ejecuta antes de realizar el enlace doble.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,23 @@ import { Employee } from '../employee/employee.model';
 
 export class HomeComponent {
   languages = ['English', 'Spanish', 'Other'];
-  employee = new Employee('', '',false,'','','');
+  employee = new Employee('', '',false,'w2','English','');
+
+  firstNameToUpperCase(value: string) {
+    this.employee.firstName = this.firstLetterToUpperCaseOtherToLowerCase(value);
+  }
+
+  lastNameToUpperCase(value: string) {
+    this.employee.lastName = this.firstLetterToUpperCaseOtherToLowerCase(value);
+  }
+
+  private firstLetterToUpperCaseOtherToLowerCase(value: string) {
+    if (value.length > 0) {
+      return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+    } else {
+      return value;
+    }
+  }
 
 }
 
